ab12ac1d3f7fc9c478afed33de18f47c83deec7d
Vinicius Mendite <vinicius@sharbe.com.br>
2021-08-02 14:49:07 -0300

    config theme of application

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+`;
