daeacefa259c207f96b7edbc51d4f14460f7171a
Vinicius Mendite <vinicius@sharbe.com.br>
2021-08-09 16:54:19 -0300

    screen scheduling details

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 
 import theme from './src/styles/theme';
 
-import { Scheduling } from './src/pages/Scheduling';
+import { SchedulingDetails } from './src/pages/SchedulingDetails';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +31,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 }
