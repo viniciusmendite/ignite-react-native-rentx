792ca82f38285bac7f91d5ed751aa57908ea25f1
Vinicius Mendite <vinicius@sharbe.com.br>
2021-08-17 09:15:43 -0300

    prevent back

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar, StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet, BackHandler } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { RFValue } from 'react-native-responsive-fontsize';
@@ -78,6 +78,12 @@ export function Home() {
     fetchCars();
   }, []);
 
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      return true;
+    });
+  })
+
   function handleCarDetails(car: ICarDTO) {
     navigation.navigate('CarDetails', { car })
   }
@@ -96,7 +102,10 @@ export function Home() {
       <Header>
         <HeaderContent>
           <Logo width={RFValue(108)} height={RFValue(12)} />
-          <TotalCars>Total de {cars.length} carros</TotalCars>
+          {
+            !loading &&
+            <TotalCars>Total de {cars.length} carros</TotalCars>
+          }
         </HeaderContent>
       </Header>
 
